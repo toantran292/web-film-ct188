f51f87e957f8e18b537c1466058a8014e193e773
toantran292 <tranthaitoan998@gmail.com>
2023-11-14 15:21:38 +0700

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
-import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import * as ReactDOM from 'react-dom/client'
-import App from './App'
-import './i18n'
+import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import * as ReactDOM from "react-dom/client";
+import App from "./App";
+import "./i18n";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "./main.css";
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/theme.js
+++ src/theme.js
@@ -42,6 +42,22 @@ const borderRadius = {
     },
 }
 
+const fontSizes = {
+    xs: "0.75rem",
+    sm: "0.875rem",
+    md: "1rem",
+    lg: "1.125rem",
+    xl: "1.25rem",
+    "2xl": "1.5rem",
+    "3xl": "1.875rem",
+    "4xl": "2.25rem",
+    "5xl": "3rem",
+    "6xl": "3.75rem",
+    "7xl": "4.5rem",
+    "8xl": "6rem",
+    "9xl": "8rem",
+  }
+
 const zIndices = {
     hide: -1,
     auto: 'auto',
@@ -58,12 +74,58 @@ const zIndices = {
     tooltip: 1800,
 }
 
+const breakpoints = {
+    base: "0em", // 0px
+    sm: "30em", // ~480px. em is a relative unit and is dependant on the font size.
+    md: "48em", // ~768px
+    lg: "62em", // ~992px
+    xl: "80em", // ~1280px
+    "2xl": "96em", // ~1536px
+  };
 
 const config = {
     initialColorMode: 'light',
     useSystemColorMode: false,
 }
 
-const theme = extendTheme({ sizes, borderRadius, zIndices, config })
+const spacing = {
+    space: {
+      px: "1px",
+      0.5: "0.125rem",
+      1: "0.25rem",
+      1.5: "0.375rem",
+      2: "0.5rem",
+      2.5: "0.625rem",
+      3: "0.75rem",
+      3.5: "0.875rem",
+      4: "1rem",
+      5: "1.25rem",
+      6: "1.5rem",
+      7: "1.75rem",
+      8: "2rem",
+      9: "2.25rem",
+      10: "2.5rem",
+      12: "3rem",
+      14: "3.5rem",
+      16: "4rem",
+      20: "5rem",
+      24: "6rem",
+      28: "7rem",
+      32: "8rem",
+      36: "9rem",
+      40: "10rem",
+      44: "11rem",
+      48: "12rem",
+      52: "13rem",
+      56: "14rem",
+      60: "15rem",
+      64: "16rem",
+      72: "18rem",
+      80: "20rem",
+      96: "24rem",
+    },
+  };
+
+const theme = extendTheme({ breakpoints, sizes, borderRadius, zIndices, fontSizes, config, ...spacing })
 
 export default theme
